4d256299596bfc46e4a9d7eb4b32e287c9275cd9
Siverus11 <siverusblack1108@gmail.com>
2018-10-20 17:13:53 +0300

    My first commit

---
+++ Program.java
@@ -0,0 +1,13 @@
+package qwerty;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Hello World!");
+		scan.close();
+	}
+
+}
